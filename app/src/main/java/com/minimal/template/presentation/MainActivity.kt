c764d4008acf3f47d549e187d8a71a9cff7fc7e0
Minjae Kim <alswo5052@gmail.com>
2021-12-08 00:05:17 +0900

    chore: set main screen system ui

--- app/src/main/java/com/minimal/template/presentation/MainActivity.kt
+++ app/src/main/java/com/minimal/template/presentation/MainActivity.kt
@@ -3,21 +3,30 @@ package com.minimal.template.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.systemBarsPadding
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.minimal.template.presentation.ui.theme.MinimalTemplateTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             MinimalTemplateTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                ProvideWindowInsets {
+                    MinimalTemplate()
                 }
             }
         }
@@ -25,14 +34,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MinimalTemplate() {
+    val systemUiController = rememberSystemUiController()
+    val systemUiColor = MaterialTheme.colors.background
+    val useDarkIcons = MaterialTheme.colors.isLight
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = systemUiColor,
+            darkIcons = useDarkIcons
+        )
+    }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(
+            modifier = Modifier
+                .systemBarsPadding()
+                .fillMaxSize()
+        ) {
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MinimalTemplateTheme {
-        Greeting("Android")
+        MinimalTemplate()
     }
 }
