b1cfdd1f651371786884b6972ea577b8cee3d5d2
Minjae Kim <alswo5052@gmail.com>
2022-05-03 22:05:14 +0900

    feat: Add coroutines module

---
+++ app/src/main/java/com/minimal/template/di/CoroutinesModule.kt
@@ -0,0 +1,34 @@
+package com.minimal.template.di
+
+import com.minimal.template.domain.di.DefaultDispatcher
+import com.minimal.template.domain.di.IoDispatcher
+import com.minimal.template.domain.di.MainDispatcher
+import com.minimal.template.domain.di.MainImmediateDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutinesModule {
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+    @MainImmediateDispatcher
+    @Provides
+    fun providesMainImmediateDispatcher(): CoroutineDispatcher = Dispatchers.Main.immediate
+}
---
+++ domain/src/main/java/com/minimal/template/domain/di/CoroutinesQualifiers.kt
@@ -0,0 +1,23 @@
+package com.minimal.template.domain.di
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainImmediateDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class ApplicationScope
